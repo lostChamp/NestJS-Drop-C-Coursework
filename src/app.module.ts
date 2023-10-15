25de2cd999f15c9c13664297fbeac6787ea37e87
Kostya <lost.champ2007@gmail.com>
2023-10-16 02:34:42 +0500

    Add all of apps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { LotController } from './lot/lot.controller';
+import { LotModule } from './lot/lot.module';
+import { RoleModule } from './role/role.module';
+import { ProfileController } from './profile/profile.controller';
+import { ProfileModule } from './profile/profile.module';
+import { ServiceService } from './service/service.service';
+import { ServiceModule } from './service/service.module';
+import { ManufacturerModule } from './manufacturer/manufacturer.module';
+import { ManufacturerService } from './manufacturer/manufacturer.service';
+import { ManufacturerController } from './manufacturer/manufacturer.controller';
+import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    LotModule,
+    RoleModule,
+    ProfileModule,
+    ServiceModule,
+    ManufacturerModule,
+    OrderModule,
+  ],
+  controllers: [
+    AuthController,
+    LotController,
+    ProfileController,
+    ManufacturerController,
+  ],
+  providers: [AuthService, ServiceService, ManufacturerService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 8000;
+  await app.listen(PORT);
+  Logger.log(`🚀 Application API is running on: http://localhost:${PORT}`);
 }
 bootstrap();
