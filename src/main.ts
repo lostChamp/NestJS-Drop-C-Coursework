e1d1b2af29f5b4177fade812d07a237b2e074a89
Kostya <lost.champ2007@gmail.com>
2023-10-16 02:43:29 +0500

    Add hbs, pg and typeorm

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(__dirname + '..' + 'public');
+  app.setBaseViewsDir(__dirname + '..' + 'views');
+  app.setViewEngine('hbs');
+
   const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
   Logger.log(`🚀 Application API is running on: http://localhost:${PORT}`);
