34a72739e4f46de2a0463e5bebedc731294a6367
Kostya <lost.champ2007@gmail.com>
2023-10-25 14:15:34 +0500

    Auth forms and take data form

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, Res } from "@nestjs/common";
 import { Response } from "express";
+import { CreateUserDto } from "../user/dto/create.user.dto";
+import { LoginDto } from "./dto/login.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -8,10 +10,24 @@ export class AuthController {
   async registration(@Res() res: Response) {
     return res.render("sign-up");
   }
+  @Post("/sign-up/reg")
+  async regUser(
+    @Body() info: CreateUserDto,
+    @Res() res: Response
+  ) {
+    console.log(info);
+    return res.redirect("/auth/login");
+  }
+
 
   @Get("login")
   async login(@Res() res: Response) {
     return res.render("login");
+  }
+
+  @Post("/login/gateway")
+  async loginGateway(@Body() info: LoginDto) {
+    console.log(info);
   }
 
 }
